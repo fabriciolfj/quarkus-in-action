5282fd4bc3482bdab2ede2e670904329807a1224
fabricio jacob <fabrluc@outlook.com>
2024-05-30 20:23:05 -0300

    add

--- src/main/java/com/github/GreetingResource.java
+++ src/main/java/com/github/GreetingResource.java
@@ -3,14 +3,38 @@ package com.github;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.security.Principal;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "greeting")
+    String greeting;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return greeting;
+    }
+
+    // import jakarta.ws.rs.core.Context;
+// import jakarta.ws.rs.core.SecurityContext;
+// import java.security.Principal;
+
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String whoAmI(@Context SecurityContext securityContext) {
+        Principal userPrincipal = securityContext.getUserPrincipal();
+        if (userPrincipal != null) {
+            return userPrincipal.getName();
+        }
+
+        return "anonymous2";
     }
 }
